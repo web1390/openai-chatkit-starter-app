706a1070b1a12138ba3835a280c8fc9a287a7445
web1390 <web1390@gmail.com>
2025-10-16 07:59:40 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import StableChat from "@/components/StableChat";
-
+import App from "./App";
 
 export default function Home() {
-  return (
-    <main style={{maxWidth: 840, margin: "0 auto", padding: 24}}>
-      <h1 style={{fontSize: 24, marginBottom: 12}}>وب‌چت WinSun Studio</h1>
-      <StableChat />
-    </main>
-  );
+  return <App />;
 }
