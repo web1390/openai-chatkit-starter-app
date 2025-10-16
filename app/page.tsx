0c3d684b6f15f3e44fb810018e551b5687e20a0b
web1390 <web1390@gmail.com>
2025-10-16 08:20:53 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import App from "./App";
+import DebugOverlay from "@/components/DebugOverlay";
 
 export default function Home() {
-  return <App />;
+  return (
+    <>
+      <App />
+      <DebugOverlay />
+    </>
+  );
 }
