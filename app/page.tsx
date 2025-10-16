62434689872a4801d6526f829b0be4e9e0e6bdd2
web1390 <web1390@gmail.com>
2025-10-16 08:34:40 +0330

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
-import App from "./App";
+"use client";
+
+import dynamic from "next/dynamic";
+import ErrorBoundary from "@/components/ErrorBoundary";
 import DebugOverlay from "@/components/DebugOverlay";
+
+// App را فقط در کلاینت لود کن تا SSR/Hydration دخالت نکند
+const ClientApp = dynamic(() => import("./App"), { ssr: false });
 
 export default function Home() {
   return (
     <>
-      <App />
+      <ErrorBoundary>
+        <ClientApp />
+      </ErrorBoundary>
       <DebugOverlay />
     </>
   );
